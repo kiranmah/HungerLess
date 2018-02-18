59d6ff4f189320ff16c8c05b606ae6045b37597a
ShantaSukhoo <shantasukhoo@gmail.com>
2018-02-17 20:40:48 -0400

    page added and base template under changes
    User shanta also addes

--- hunger_less/hunger_less/post/urls.py
+++ hunger_less/hunger_less/post/urls.py
@@ -1,7 +1,13 @@
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.views.generic import TemplateView
+from django.views import defaults as default_views
 
 from . import views
 
 urlpatterns = [
+       url(r'^$', TemplateView.as_view(template_name='posts/viewposts.html'), name='posts'),
 
 ]
